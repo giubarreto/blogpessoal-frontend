04354a7e8dcc646cf0e8e33be3af0db5f2ecb134
giulia <giuliabarreto2@gmail.com>
2025-07-28 12:36:58 -0300

    add footer, navbar,login e cadastro

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+
+function Footer() {
+
+    const data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Giulia Barreto | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                   <div className='flex gap-2'>
+	<a href="https://www.linkedin.com/in/giulia-barreto-849a612b0/" target="_blank">
+    	<LinkedinLogo size={48} weight='bold' />
+    </a>
+    <a href="https://www.instagram.com/giu_sbarreto?igsh=d25vbno0bGg1MGxp&utm_source=qr" target="_blank">
+    	<InstagramLogo size={48} weight='bold' />
+    </a>
+    <a href="https://www.facebook.com/share/1AjpWnzNPX/?mibextid=wwXIfr" target="_blank">
+    	<FacebookLogo size={48} weight='bold' />
+	</a>
+</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
