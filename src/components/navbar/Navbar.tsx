0b874fc9e8fff43e08a275bc41a4e1ca06c82859
giulia <giuliabarreto2@gmail.com>
2025-07-30 15:21:23 -0300

    add crud tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,9 +25,9 @@ function Navbar() {
 
                     <div className='flex gap-4'>
                         Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                         perfil
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
